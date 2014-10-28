3efae3cd9729304727be1982eeafff33fa516d8f
Hunter Meyer <huntermeyer@live.com>
2014-10-28 15:33:11 -0400

    Configure paperclip

---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,3 @@
+Paperclip::Attachment.default_options[:url] = 'debsday.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:filename'
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
