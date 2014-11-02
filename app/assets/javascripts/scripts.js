d4f2a5a5d7f9049f1b450c9b547a83d4000cbed9
Hunter Meyer <huntermeyer@live.com>
2014-11-02 02:41:30 -0500

    Use jquery to trigger page redirect on button click

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   $('#msg-pic').click(function() { $('#hide-pic').trigger('click'); });
   $('#msg-vid').click(function() { $('#hide-vid').trigger('click'); });
+  $('#send-another').click(function() { window.location = '/'; });
 
   $('#msg-submit').click(function() {
     $(this).hide();
