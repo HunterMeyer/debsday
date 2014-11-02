f812197be46d918ce061a54ed8abe711a8ab7da3
Hunter Meyer <huntermeyer@live.com>
2014-11-02 02:18:25 -0500

    Render error messages on form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,12 +7,9 @@ class MessagesController < ApplicationController
   def create
     @message = Message.create(message_params)
     if @message.save
-      flash[:success] = 'Thank you'
       redirect_to root_path
     else
       render 'new'
-      # flash[:warning] = @message.errors.each { |x,y| puts "#{x.to_s} #{y.to_s}".titleize }
-      flash[:warning] = 'Something went wrong'
     end
   end
 
